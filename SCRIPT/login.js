57ee11523407bf26c32ec86927d5d1f9a70317a5
tan-vir828 <almuntakimtanvir@gmail.com>
2025-09-25 00:36:29 +0600

    add money

--- SCRIPT/login.js
+++ SCRIPT/login.js
@@ -1,21 +1,19 @@
-document.getElementById("login-btn").addEventListener("click", function(event){
-// to prevent clear the form or redirect
+document.getElementById("login-btn").addEventListener("click", function (event) {
     event.preventDefault();
-
-    const accountNumber = document.getElementById("account-number").value;
-  
-    const pin = document.getElementById("account-pin").value;
-    if(accountNumber.length == 11){
-        if(pin == 1234){
-            window.location.href = "./main.html"
+    const acountNunber = document.getElementById("Acount-nunber").value;
+    const pin = document.getElementById("pin").value;
+    const convertedPin=parseInt(pin)
+    if (acountNunber.length === 11) {
+      
+        if (convertedPin===1234) {
+            window.location.href="./main.html"
         }
-        else{
-        alert("Pin is not valid")
+        else {
+            alert("pin tikh nai ")
+        }
     }
-    }
-
-    else{
-        alert("Acount is not valid")
+    else {
+       alert("need valid acount number")
     }
 
 })
