e379c0df50485ff5ac4e7bf1dcd47c16f4c00b5e
tan-vir828 <almuntakimtanvir@gmail.com>
2025-09-25 02:23:16 +0600

    jani na

--- SCRIPT/addmoney.js
+++ SCRIPT/addmoney.js
@@ -1,4 +1,6 @@
 document.getElementById('add-money').addEventListener('click',function(event){
     event.preventDefault();
     const amount = document.getElementById("amount").value;
+
+    const convertedAmount = parseFloat(amount);
 })
