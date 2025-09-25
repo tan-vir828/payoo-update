fd0770471300236881d30bbbca2a5a0837919ece
tan-vir828 <almuntakimtanvir@gmail.com>
2025-09-25 09:43:51 +0600

    can add more  money

--- SCRIPT/addmoney.js
+++ SCRIPT/addmoney.js
@@ -1,22 +1,22 @@
-document.getElementById('add-money').addEventListener('click', function (event) {
+document.getElementById('add-money').addEventListener('click',function(event){
     event.preventDefault();
-
     const amount = document.getElementById("amount").value;
+
     const convertedAmount = parseFloat(amount);
 
     const pin = document.getElementById("pin").value;
+     
+    const mainBalance = document.getElementById("main-balance").innerText;
 
-    
-    const mainBalanceEl = document.querySelector(".main-balance");
-    const convertedMainbalance = parseFloat(mainBalanceEl.innerText);
+    const convertedMainbalance = parseFloat(mainBalance);
 
     const convertedPin = parseInt(pin);
 
-    if (convertedPin === 1234) {
+    if(convertedPin === 1234){
         const sum = convertedMainbalance + convertedAmount;
-      
-        mainBalanceEl.innerText = sum;
-    } else {
-        alert("pin error")
+        document.getElementById("main-balance").innerText = sum;
     }
-});
+    else{
+        console.log("pin error");
+    }
+})
