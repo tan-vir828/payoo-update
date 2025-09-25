2abffb9b334596636c25e6f993f11bbe8ad6c894
tan-vir828 <almuntakimtanvir@gmail.com>
2025-09-25 09:39:42 +0600

    can add money

--- SCRIPT/addmoney.js
+++ SCRIPT/addmoney.js
@@ -1,6 +1,22 @@
-document.getElementById('add-money').addEventListener('click',function(event){
+document.getElementById('add-money').addEventListener('click', function (event) {
     event.preventDefault();
-    const amount = document.getElementById("amount").value;
 
+    const amount = document.getElementById("amount").value;
     const convertedAmount = parseFloat(amount);
-})
+
+    const pin = document.getElementById("pin").value;
+
+    
+    const mainBalanceEl = document.querySelector(".main-balance");
+    const convertedMainbalance = parseFloat(mainBalanceEl.innerText);
+
+    const convertedPin = parseInt(pin);
+
+    if (convertedPin === 1234) {
+        const sum = convertedMainbalance + convertedAmount;
+      
+        mainBalanceEl.innerText = sum;
+    } else {
+        alert("pin error")
+    }
+});
