babf73871e83fe946cad966cd79099f2c95f293c
tan-vir828 <almuntakimtanvir@gmail.com>
2025-09-26 22:11:30 +0600

    BONUS

--- SCRIPT/bonus.js
+++ SCRIPT/bonus.js
@@ -1,11 +1,11 @@
-document.getElementById("bonus-btn").addEventListener("click", function (event) {
+document.getElementById("bonus").addEventListener("click", function (event) {
     event.preventDefault();
 
     const coupon = document.getElementById("coupon-code").value.trim();
     const mainBalanceElement = document.getElementById("main-balance");
     let balance = parseFloat(mainBalanceElement.innerText);
 
-    // fixed coupons with fixed amounts
+    
     const coupons = {
         "BONUS100": 100,
         "BONUS500": 500,
@@ -15,7 +15,7 @@ document.getElementById("bonus-btn").addEventListener("click", function (event) 
     if (coupons[coupon]) {
         balance += coupons[coupon];
         mainBalanceElement.innerText = balance.toFixed(2);
-        alert(`🎉 Congrats! ${coupons[coupon]} added to your balance.`);
+        // alert(`🎉 Congrats! ${coupons[coupon]} added to your balance.`);
         document.getElementById("coupon-code").value = "";
     } else {
         alert("❌ Invalid or expired coupon!");
--- SCRIPT/toogle.js
+++ SCRIPT/toogle.js
@@ -1,19 +1,29 @@
  document.getElementById("cashout").style.display = "none";
  document.getElementById("transfer").style.display = "none";
+ document.getElementById("bonus").style.display = "none";
 
 document.getElementById("add-money-box").addEventListener("click",function(){
     document.getElementById("cashout").style.display = "none";
     document.getElementById("addmoney").style.display = "block";
     document.getElementById("transfer").style.display = "none";
+    document.getElementById("bonus").style.display = "none";
 })
 document.getElementById("cash-out-box").addEventListener("click",function(){
     document.getElementById("cashout").style.display = "block";
     document.getElementById("addmoney").style.display = "none";
     document.getElementById("transfer").style.display = "none";
+    document.getElementById("bonus").style.display = "none";
 })
 document.getElementById("transfer-btn-box").addEventListener("click",function(){
     document.getElementById("cashout").style.display = "none";
     document.getElementById("addmoney").style.display = "none";
     document.getElementById("transfer").style.display = "block";
+    document.getElementById("bonus").style.display = "none";
+})
+document.getElementById("bonus-btn-box").addEventListener("click",function(){
+    document.getElementById("cashout").style.display = "none";
+    document.getElementById("addmoney").style.display = "none";
+    document.getElementById("transfer").style.display = "none";
+    document.getElementById("bonus").style.display = "block";
 })
 
